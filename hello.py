ae5bf2af533e11fa261637fa0c82184dbc299e4a
newwind3 <newwind3@naver.com>
2016-05-27 14:50:19 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-print("Hello world")
-print("Tell your world")
 //For command line git tutorial
 //Local repository modifcation
 //GitHub modifcation
+print("Hello world")
+print("Tell your world")
+print{"Tell my world")
 
